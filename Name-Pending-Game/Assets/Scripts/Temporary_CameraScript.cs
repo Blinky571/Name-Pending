2433be4789a6cc6465bd5687fa9e0ee5d3199370
Blinky <emiiru.re@gmail.com>
2023-02-10 13:25:19 -0800

    nothing

--- Name-Pending-Game/Assets/Scripts/Temporary_CameraScript.cs
+++ Name-Pending-Game/Assets/Scripts/Temporary_CameraScript.cs
@@ -21,7 +21,7 @@ public class Temporary_CameraScript : MonoBehaviour
     void Update()
     {
 
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y +4, transform.position.z);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y +3, transform.position.z);
         
     }
 }
