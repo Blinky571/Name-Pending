dc83c1c5ea02a4d3ad8a045e958a6b5c69429930
Blinky <emiiru.re@gmail.com>
2023-02-10 11:35:15 -0800

    Powercells and other stuff

--- Name-Pending-Game/Assets/Scripts/Movement/Player_Controller.cs
+++ Name-Pending-Game/Assets/Scripts/Movement/Player_Controller.cs
@@ -9,35 +9,43 @@ public class Player_Controller : MonoBehaviour
 
     #region Player Speeds
     [SerializeField]
-    private float _playerSpeed = 140f;
+    private float _playerSpeed;
+    [SerializeField]
+    private float _jumpHeight;
     #endregion
 
+    private bool _isGrounded;
     private bool _FacingRight = true;
     private Vector2 _playerVelocity;
     private bool _isJogging;
+
 
     private void Awake()
     {
         animator = gameObject.GetComponent<Animator>();
         rb = gameObject.GetComponent<Rigidbody2D>();        
     }
-
-
     private void Update()
     {
         float moveInput = Input.GetAxisRaw("Horizontal");
         transform.Translate(_playerVelocity * Time.deltaTime);
         animator.SetBool("isJogging", _isJogging);
-
         if (moveInput != 0)
         {
             _playerVelocity.x = moveInput * _playerSpeed;
             _isJogging = true;
+            animator.SetBool("isWaiting", false);
         }
         else
         {
             _playerVelocity.x = 0f;
             _isJogging = false;
+        }
+
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            rb.AddForce(new Vector2(0, _jumpHeight));
+            animator.SetBool("isWaiting", false);
         }
 
         if (moveInput < 0 && _FacingRight)
@@ -49,7 +57,6 @@ public class Player_Controller : MonoBehaviour
             Flip();
         }
     }
-
     void Flip()
     {
         Vector2 _currentScale = gameObject.transform.localScale;
@@ -57,5 +64,15 @@ public class Player_Controller : MonoBehaviour
         gameObject.transform.localScale = _currentScale;
 
         _FacingRight = !_FacingRight;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        _isGrounded = false;
     }
 }
