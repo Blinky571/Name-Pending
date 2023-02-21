b817a9cc2915cfbb7cfbcdc91a07ee629a46517f
LuffeB <uffeulvbjerregaard@gmail.com>
2023-02-21 10:57:02 -0800

    gwa

--- Name-Pending-Game/Assets/Scripts/Camera scipts/CameraController.cs
+++ Name-Pending-Game/Assets/Scripts/Camera scipts/CameraController.cs
@@ -2,12 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera[] cameras;
     [SerializeField] private CinemachineVirtualCamera thisCamera;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         for (int i = 0; i < cameras.Length; i++)
