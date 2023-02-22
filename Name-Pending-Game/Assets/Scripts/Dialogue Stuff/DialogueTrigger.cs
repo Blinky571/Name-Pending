1219b6e67e670a065cf74a16310b150db16da762
Blinky <emiiru.re@gmail.com>
2023-02-22 11:08:09 -0800

    ehe

--- Name-Pending-Game/Assets/Scripts/Dialogue Stuff/DialogueTrigger.cs
+++ Name-Pending-Game/Assets/Scripts/Dialogue Stuff/DialogueTrigger.cs
@@ -2,12 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueTrigger : MonoBehaviour
+public class DialogueTrigger : MonoBehaviour, IInteractable
 {
     public Dialogue dialogue;
 
+    public string InteractionPrompt { get; }
+
+    public void ElevatorMove()
+    {
+        FindObjectOfType<DialogueManager>().StartDialouge(dialogue);
+    }
+
+    public void FlagSpot(int flagsPlaced)
+    {
+    }
+
+    public int Interact(Interactor interactor, int powerCellsAcquired)
+    {
+        return powerCellsAcquired;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-          FindObjectOfType<DialogueManager>().StartDialouge(dialogue);
+
     }
 }
